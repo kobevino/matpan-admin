6201e43b2ae64d176430680439b6aa46ca3fefa7
kobe <kobe8vino24@gmail.com>
2026-01-25 11:42:25 +0900

    chore: add lint rule

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-3xl font-bold text-gray-900">Matpan Admin</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
